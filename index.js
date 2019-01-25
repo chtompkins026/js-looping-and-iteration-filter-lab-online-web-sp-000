40d006f01b79a1d78b5314a62b76188bddf5dc97
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-25 14:55:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 
 function findMatching(drivers, string) { 
   return drivers.filter(function(name) {
-    return string === name; 
+    return string.toUpperCase() === name.toUpperCase(); 
   });
 }
