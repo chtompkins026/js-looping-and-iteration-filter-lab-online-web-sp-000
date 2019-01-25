335a8c137f07999a6536f2bd8eedeaadc63b358e
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-25 15:05:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function fuzzyMatch(drivers, string){
 
 function matchName(drivers, string){ 
  return drivers.filter(function(el){ 
-   el.name == string; 
+   return el.name == string; 
  }) 
 }
